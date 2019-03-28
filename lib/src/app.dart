431710058b78e59ef6fa0190e6a655fac12de2f6
vineesh <vineesh27vk@gmail.com>
2019-03-28 16:02:27 +0530

    added authscreen

---
+++ lib/src/app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:od_register/src/screens/auth/auth_screen.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: AuthScreen());
+  }
+}
