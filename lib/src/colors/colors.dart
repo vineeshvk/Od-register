c2ade5624a04530ae8007957a4e59f70ed9aebdb
vineesh <vineesh27vk@gmail.com>
2019-03-29 09:38:17 +0530

    added color constants

---
+++ lib/src/colors/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color.fromRGBO(19, 43, 75, 1);
+const PRIMARY_DARK_COLOR = Color.fromRGBO(15, 26, 39, 1);
+const WHITE_COLOR = Colors.white;
+const GREY_COLOR = Color.fromRGBO(229, 229, 229, 1);
+const ACCENT_COLOR = Color.fromRGBO(0, 112, 255, 1);
