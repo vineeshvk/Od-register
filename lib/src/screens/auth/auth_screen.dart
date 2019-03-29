76405e1e78918c4d3daf89dcdb6c5bf37ba339fe
ThalapthySiva <sivaramsiva16@gmail.com>
2019-03-29 09:27:16 +0530

    checking bg color

--- lib/src/screens/auth/auth_screen.dart
+++ lib/src/screens/auth/auth_screen.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
-class AuthScreen extends StatelessWidget{
+class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(15, 26, 39, 1),
+      body: _welcomeText(),
+    );
+  }
+
+  Widget _welcomeText() {
+    return Text(
+      "Welcome",
+      style: TextStyle(fontFamily: 'Raleway'),
+    );
   }
 }
