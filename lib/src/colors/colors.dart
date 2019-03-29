d17074338197ffbdc2c36b2d719e0e6472799e64
ThalapthySiva <sivaramsiva16@gmail.com>
2019-03-29 12:14:49 +0530

    login screen

--- lib/src/colors/colors.dart
+++ lib/src/colors/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const PRIMARY_COLOR = Color.fromRGBO(25, 25, 25, 0.32);
+const PRIMARY_COLOR = Color.fromRGBO(81, 18, 145, 1);
 const WHITE_COLOR = Colors.white;
 const GREY_COLOR = Color.fromRGBO(229, 229, 229, 1);
 const ACCENT_COLOR = Color.fromRGBO(0, 81, 185, 1);
--- lib/src/screens/auth/auth_screen.dart
+++ lib/src/screens/auth/auth_screen.dart
@@ -1,18 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:od_register/src/colors/colors.dart';
 
 class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(15, 26, 39, 1),
-      body: _welcomeText(),
-    );
+    return Scaffold(backgroundColor: PRIMARY_COLOR, body: _bodyContent());
+  }
+
+  Widget _bodyContent() {
+    return Container(
+        child: ListView(
+      children: <Widget>[
+        Container(margin: EdgeInsets.only(top: 82)),
+        _welcomeText(),
+        Container(margin: EdgeInsets.only(top: 20)),
+        _descpText(),
+        Container(margin: EdgeInsets.only(top: 100)),
+        _loginText(),
+        Container(margin: EdgeInsets.only(top: 30)),
+        _usernameTextField(),
+        Container(margin: EdgeInsets.only(top: 10)),
+        _userpasswordField(),
+        Container(margin: EdgeInsets.only(top: 10)),
+        _loginButton()
+      ],
+    ));
   }
 
   Widget _welcomeText() {
     return Text(
       "Welcome",
-      style: TextStyle(fontFamily: 'Raleway'),
+      style: TextStyle(fontFamily: 'Raleway', fontSize: 36, color: WHITE_COLOR),
     );
+  }
+
+  Widget _descpText() {
+    return Text(
+      "On duty passes made simple.",
+      style: TextStyle(fontSize: 18, color: GREY_COLOR),
+    );
+  }
+
+  Widget _loginText() {
+    return Text(
+      "Login to your account",
+      style: TextStyle(fontFamily: 'Raleway', fontSize: 24, color: WHITE_COLOR),
+    );
+  }
+
+  Widget _usernameTextField() {
+    return TextField(
+        style: TextStyle(fontFamily: 'Raleway', color: WHITE_COLOR),
+        decoration: InputDecoration(
+          counterText: "Students enter register number. Teachers enter email.",
+          counterStyle: TextStyle(color: GREY_COLOR),
+          labelText: "username",
+          labelStyle: TextStyle(
+              fontFamily: 'Raleway', fontSize: 16, color: WHITE_COLOR),
+          focusedBorder: _border(),
+          hasFloatingPlaceholder: true,
+          enabledBorder: _border(),
+        ));
+  }
+
+  OutlineInputBorder _border() {
+    return OutlineInputBorder(
+        borderSide: BorderSide(color: WHITE_COLOR, width: 0),
+        borderRadius: BorderRadius.all(Radius.circular(10.0)));
+  }
+
+  Widget _userpasswordField() {
+    return TextField(
+        obscureText: true,
+        style: TextStyle(fontFamily: 'Raleway', color: WHITE_COLOR),
+        decoration: InputDecoration(
+          counterText: "Please enter your password. Default is 0000",
+          counterStyle: TextStyle(color: GREY_COLOR),
+          labelText: "password",
+          labelStyle: TextStyle(
+              fontFamily: 'Raleway', fontSize: 16, color: WHITE_COLOR),
+          focusedBorder: _border(),
+          hasFloatingPlaceholder: true,
+          enabledBorder: _border(),
+        ));
+  }
+
+  Widget _loginButton() {
+    return Container(
+        width: 320,
+        height: 50,
+        child: RaisedButton(
+          onPressed: () {
+            print("hai");
+          },
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          elevation: 10,
+          color: ACCENT_COLOR,
+          child: Text(
+            "LOG IN",
+            style: TextStyle(color: WHITE_COLOR),
+          ),
+        ));
   }
 }
